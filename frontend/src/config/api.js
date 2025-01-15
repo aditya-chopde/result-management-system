41024d7ed164855205f9df9752af5203999a610b
Aditya Digambar Chopde <164034657+aditya-chopde@users.noreply.github.com>
2025-01-15 19:39:51 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://result-backend-j3x7.onrender.com"
+    baseURL: "https://result-f6qg.onrender.com/"
 })
 
 export default api;
